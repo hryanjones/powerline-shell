257ed8611802a54663b91dba9917b8aefbc76c4c
H. Ryan Jones <hryanjones@gmail.com>
2013-07-20 08:25:36 -0700

    Removing spaces and adding /s so that CWD can be copied

--- segments/cwd.py
+++ segments/cwd.py
@@ -17,13 +17,17 @@ def add_cwd_segment():
     names = get_short_path(cwd.decode('utf-8'))
 
     max_depth = powerline.args.cwd_max_depth
+    # better to try using xwininfo to get actual width
+    # only issue here is that you may not always be using X (e.g. SSH, will need to test)
+    # xwininfo -id (xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}') | grep --color=never geometry | grep -Eo "[0-9]*x[0-9]*"'
     if len(names) > max_depth:
         names = names[:2] + [u'\u2026'] + names[2 - max_depth:]
 
     if not powerline.args.cwd_only:
+        names[0] = ' /'+names[0]
         for n in names[:-1]:
-            powerline.append(' %s ' % n, Color.PATH_FG, Color.PATH_BG,
+            powerline.append('%s/' % n, Color.PATH_FG, Color.PATH_BG,
                     powerline.separator_thin, Color.SEPARATOR_FG)
-    powerline.append(' %s ' % names[-1], Color.CWD_FG, Color.PATH_BG)
+    powerline.append('%s ' % names[-1], Color.CWD_FG, Color.PATH_BG)
 
 add_cwd_segment()
